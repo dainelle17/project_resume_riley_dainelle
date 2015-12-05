8cfa0090e9de3a464a581083100de18674d9b900
Dainelle Riley <dainellecriley@ufl.edu>
2015-12-04 21:32:47 -0500

    add jquery slider plugin

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+            $('#fullpage').fullpage({
+                anchors: ['firstPage'],
+                sectionsColor: ['#4A6FB1'],
+                autoScrolling: false,
+                css3: true,
+                fitToSection: false
+            });
+        });
