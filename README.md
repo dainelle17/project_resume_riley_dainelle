d7df7e84830f56bde28353c0a987a323029b8964
Dainelle Riley <dainellecriley@ufl.edu>
2015-12-04 22:46:15 -0500

    add lettering

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 This plugin was used to create the single section slider which is located at the top of the page. The user can interact with the slider by clicking on the arrows to review my job experience.  
 
+[Lettering Plugin](https://github.com/davatron5000/Lettering.js)
+
+This plugin is an effect that effects the lettering. I utilized a small one on the "bye" phrase at the bottom of my page where you will notice that it is at a tilt. 
--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ $(document).ready(function() {
                 fitToSection: false
             });
         });
+
+
+
+    $(document).ready(function() {
+            $("#demo1 h1").lettering();
+            $("#demo2 h1").lettering('words');
+            $("#demo3 p").lettering('lines');
+            $("#demo4 h1").lettering('words').children("span").lettering();
+            $("#demo5 h1").lettering().children("span").css({'display':'inline-block', '-webkit-transform':'rotate(-25deg)'});
+        });
+
+$("#demo5 h1").lettering() .children("span").css({'display':'inline-block', '-webkit-transform':'rotate(-25deg)'});
